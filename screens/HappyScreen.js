dc6a71529e18bb9fcc9b337423ac24a921ed8c57
“Jamie <jamiedevine77@gmail.com>
2023-03-11 17:39:10 -0600

    arrays

--- screens/HappyScreen.js
+++ screens/HappyScreen.js
@@ -1,113 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-elements";
-import happyAffirm from "../components/happyAffirm";
-import React, { useState } from "react";
+import {happyAffirm} from "../components/happyAffirm";
 
-
-
-function randomHappy() {
-    
-   
-  const happyAffirm= [
-    {
-        id: 0,
-        text:"I am positive. I am loved. I am enough"
-    },
-    {
-        id: 1,
-        text:"I start every day with gratitude and thanks"
-    },
-    {
-        id: 2,
-        text:"Today is full of opportunity"
-    },
-    {
-        id: 3,
-        text:"I am grateful for what I have"
-    },
-    {
-        id: 4,
-        text:"I am confidently making choices that will create a better future"
-    },
-    {
-        id: 5,
-        text:"Today I am working toward creating the life of my dreams"
-    },
-    {
-        id: 6,
-        text:"I have everything I need"
-    },
-    {
-        id: 7,
-        text:"Life is full of meaning"
-    },
-    {
-        id: 8,
-        text:"I will make the most of this day"
-    },
-    {
-        id: 9,
-        text:"Today I will be fabulous"
-    },
-    {
-        id: 10,
-        text:"I am positive and create joy and happiness for others"
-    },
-    {
-        id: 11,
-        text:"I have a positive attitude "
-    },
-    {
-        id: 12,
-        text:"I accept with an open heart everything that comes"
-    },
-    {
-        id: 13,
-        text:"I feel healthy, wealthy, and wise"
-    },
-    {
-        id: 14,
-        text:"I focus on what I can control. I let go of the rest"
-    },
-    {
-        id: 15,
-        text:"I am present, powerful, and calm"
-    },
-    {
-        id: 16,
-        text:"I am focused on my family, relationships, and career"
-    },
-    {
-        id: 17,
-        text:"I embrace being a positive, joyful, and playful"
-    },
-    {
-        id: 18,
-        text:"My courage is a strong tower that surrounds me"
-    },
-    {
-        id: 19,
-        text:"I am a confident and capable person"
-    },
-    {
-        id: 20,
-        text:"My courage is a strong tower that surrounds me"
-    },
-    {
-        id: 21,
-        text:"Every day I continue to become stronger"
-    },
-    {
-        id: 22,
-        text:"I embrace who I am"
-    },
-    ];
+function randomHappy(happyAffirm) {
   return (happyAffirm[Math.floor(Math.random() * happyAffirm.length)].text);
 }
 
 const HappyScreen = () => {
-  
-
   return (
     <View style={styles.container}>
     <Card>
@@ -118,7 +17,7 @@ const HappyScreen = () => {
         fontSize: 30,
         padding: 50,
       }}
-    >{randomHappy()}</Text>
+    >{randomHappy(happyAffirm)}</Text>
     </View>
     </Card>
     </View>
@@ -131,15 +30,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffefd5",
     alignItems: "center",
     justifyContent: "center",
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 10,
-  },
-  image: {
-    width: 120,
-    height: 120,
-    borderRadius: 50,
   },
 });
 
--- screens/SadScreen.js
+++ screens/SadScreen.js
@@ -1,114 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-elements";
-import happyAffirm from "../components/happyAffirm";
-import React, { useState } from "react";
+import {sadAffirm} from "../components/sadAffirm";
 
-
-
-function randomSad() {
-    
-   
-  const sadAffirm= [
-    {
-        id: 0,
-        text:"Not everybody will understand my situation, and it is okay"
-    },
-    {
-        id: 1,
-        text:"I am much more than what I think I am"
-    },
-    {
-        id: 2,
-        text:"Every day is a gift, and I am blessed to see today"
-    },
-    {
-        id: 3,
-        text:"I have made it this far, and I won’t stop now"
-    },
-    {
-        id: 4,
-        text:"I am loved and appreciated even when I feel like I’m not"
-    },
-    {
-        id: 5,
-        text:"I am a work in progress, and I welcome every positive change"
-    },
-    {
-        id: 6,
-        text:"The darkness is in the past, and I open my eyes to new beginnings"
-    },
-    {
-        id: 7,
-        text:"I am more than what people think I am"
-    },
-    {
-        id: 8,
-        text:"My thoughts do not shape my life"
-    },
-    {
-        id: 9,
-        text:"I am needed regardless of how worthless I feel"
-    },
-    {
-        id: 10,
-        text:"Everything will work out, and I will watch it unfold"
-    },
-    {
-        id: 11,
-        text:"I forgive myself"
-    },
-    {
-        id: 12,
-        text:"I don’t blame myself for my current situation"
-    },
-    {
-        id: 13,
-        text:"It won’t last forever, and I will come out stronger"
-    },
-    {
-        id: 14,
-        text:"I deserve love, joy, and happiness focus on what I can control. I let go of the rest"
-    },
-    {
-        id: 15,
-        text:"I deserve everything good"
-    },
-    {
-        id: 16,
-        text:"I don’t have anything to prove to myself or anybody else"
-    },
-    {
-        id: 17,
-        text:"I embrace being a positive, I am enough, and that is okay"
-    },
-    {
-        id: 18,
-        text:"This darkness won’t last forever"
-    },
-    {
-        id: 19,
-        text:"I am not perfect, and it is okay"
-    },
-    {
-        id: 20,
-        text:"I forgive myself and everyone for not being perfect"
-    },
-    {
-        id: 21,
-        text:"Every day I continue to become stronger"
-    },
-    {
-        id: 22,
-        text:"I embrace who I am"
-    },
-    
-    ]
+function randomSad(sadAffirm) {
   return (sadAffirm[Math.floor(Math.random() * sadAffirm.length)].text);
 }
 
 const SadScreen = () => {
-  
-
   return (
     <View style={styles.container}>
     <Card>
@@ -119,7 +17,7 @@ const SadScreen = () => {
         fontSize: 30,
         padding: 50,
       }}
-    >{randomSad()}</Text>
+    >{randomSad(sadAffirm)}</Text>
     </View>
     </Card>
     </View>
@@ -133,15 +31,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 15,
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 10,
-  },
-  image: {
-    width: 120,
-    height: 120,
-    borderRadius: 50,
   },
 });
 
